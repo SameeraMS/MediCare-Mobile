6743f8590a72c71162889ce88177c9255f1f2839
Sameera Madushan Silva <madushansameera499@gmail.com>
2025-02-26 01:43:06 +0530

    add new start screen logo

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,25 +3,28 @@ import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
     <Provider store={store}>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="index" />
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen 
-          name="hospital/[id]" 
-          options={{
-            headerShown: true,
-            headerTitle: 'Hospital Details',
-            headerStyle: {
-              backgroundColor: '#f8f9fa',
-            },
-          }} 
-        />
-      </Stack>
-      <StatusBar style="auto" />
+      <SafeAreaProvider>
+        <Stack screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(tabs)" />
+          <Stack.Screen
+            name="hospital/[id]"
+            options={{
+              headerShown: true,
+              headerTitle: 'Hospital Details',
+              headerStyle: {
+                backgroundColor: '#f8f9fa',
+              },
+            }}
+          />
+        </Stack>
+        <StatusBar style="auto" />
+      </SafeAreaProvider>
     </Provider>
   );
 }
